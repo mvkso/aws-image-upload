dd69d267e57b3037bc7300bc04755b01b9d993cf
Kamil Maksymowicz <maxio090124@interia.pl>
2021-03-18 18:11:21 +0100

    configuration

---
+++ src/main/java/com.maks.awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,27 @@
+package com.maks.awsimageupload.config;
+
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIA4FJSESYV6SAYCZT3",
+                "1G1f6Ge2/24EvRbXWe2SRdaiQhZ6yXCPRHru3Cdf"
+        );
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
